6a0f9d8ad3a4a5474b9bb2da0eac67a81d24dae2
TDelgado <tdelgado39@gmail.com>
2022-06-24 20:55:55 -0400

    Posts

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'home/', to:"pages#home"
+  get 'pages/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'pages#home'
+end
